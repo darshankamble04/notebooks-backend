8da5d652cfdb4c664af3562cfdbd409d16a2a386
DARSHAN S KAMBLE <darshankamble7371@gmail.com>
2021-12-06 12:01:34 +0530

    make it better

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose =require("mongoose");
 // const mongooseUrl = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
-// const mongooseUrl = "mongodb+srv://test99:test123@cluster0.dn10z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const mongooseUrl = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+const mongooseUrl = "mongodb+srv://test99:test123@cluster0.dn10z.mongodb.net/notesyarddatabase?retryWrites=true&w=majority";
+// const mongooseUrl = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
 
 const connectToMongoDB = () =>{
     mongoose.connect(mongooseUrl,()=>{
